9a2d1ecf0c583a926f1ca98147eb07e2b658fd42
catanielena <catani.elena1@gmail.com>
2021-10-19 15:48:41 +0200

    Add: snack-2_form

--- Snack-2/Snack 2.php
+++ Snack-2/Snack 2.php
@@ -1,16 +1,43 @@
 <!-- Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
 
 <?php
-    if(!empty($_GET['name']) && !empty($_GET['mail']) && !empty($_GET['age'])) {
-        $name = $_GET['name'];
-        $mail = $_GET['mail'];
-        $age = $_GET['age'];
-        if(strlen($name) > 3 && strpos($mail, '@') && strpos($mail,'.') && strlen($mail) > 4 && is_numeric($age)) {
-            echo 'Successful login';
+    $message = '';
+    function submit() {      
+        if(!empty($_GET['name']) && !empty($_GET['mail']) && !empty($_GET['age'])) {
+            $name = $_GET['name'];
+            $mail = $_GET['mail'];
+            $age = $_GET['age'];
+            if(strlen($name) > 3 && strpos($mail, '@') && strpos($mail,'.') && strlen($mail) > 4 && is_numeric($age) && $age > 0) {
+                return 'Successful login';
+            } else {
+                return 'Access Denied';
+            }
         } else {
-            echo 'Access Denied';
-        }
-    } else {
-        echo 'Attention one of the inputs is empty';
+            return 'Attention one of the inputs is empty';
+        };
+    };
+    if(isset($_GET['btnSubmit'])) {
+        $message = submit();
     }
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Snack-2</title>
+    </head>
+    <body>
+        <h1>Inserisci i tuoi dati</h1>
+        <form action="" >
+            <input type="text" name="name" placeholder="Enter your name">
+            <input type="email" name="mail" placeholder="Enter your mail">
+            <input type="number" name="age" placeholder="Enter your age">
+            <input type="submit" value="Send Request" name="btnSubmit">
+        </form>
+        <p class="message"><?php echo $message; ?></p>
+    </body>
+</html>
